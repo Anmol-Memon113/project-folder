21f7c35e582f90ea81e7b8f0510e7e3d0a99f2e6
Anmol-Memon113 <144772489+Anmol-Memon113@users.noreply.github.com>
2023-09-12 12:53:50 +0500

    My project

---
+++ asiya nazeer gogle/index.js
@@ -0,0 +1,18 @@
+function validateForm() {
+    let x = document.getElementById("name").value;
+    if (x=='muneermadni_12') {
+      alert("valid");
+    }
+    else{
+      alert("non valid");
+    }
+  }
+  function validation() {
+      let x = document.getElementById("Confirm").value;
+    if (x=='muneermadni_12') {
+      alert("valid");
+    }
+    else{
+      alert("password and confirm password not maching");                  
+    }
+  }
